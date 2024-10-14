4a0e8b5913539934ef253e2a1ab7c4dea4b994b7
Bud_Ton <d2023ujle@gmail.com>
2024-10-14 16:40:12 +0300

    Сorrection of comments

--- src/js/telCorrection.js
+++ src/js/telCorrection.js
@@ -1,7 +1,5 @@
 export default function telCorrection(telNum) {
-    telNum = telNum.replace(/^[8]/g, '+7')
-    telNum = telNum.replace(/-|\s|\(|\)/g, '')
-    return telNum;
+    return telNum.replace(/^[8]/g, '+7').replace(/-|\s|\(|\)/g, '')
 }
 // console.log(telCorrection('8925 (168 01-20'));
 // console.log(telCorrection('8(927) 000-00-00'));
